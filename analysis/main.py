a7f95df0e3379255967017d70f4f2373af4652ac
Ian Chen <ianchen3@illinois.edu>
2025-04-08 17:59:16 -0500

    experiment 1 replicates

--- analysis/main.py
+++ analysis/main.py
@@ -1,66 +1,60 @@
 # ==========================================================================================================================================
-# ==========================================================================================================================================
-# 1. Make a table or figure of average tree errors for each of the five methods for the two methods
-# 2. For 1000M1 and 1000M2
-#   (a) Which method is the most accurate?
-#   (b) Does changing the estimation method for FastTree impact accuracy? If so, how?
-#   (c) Does changing the distance correction for Neighbor Joining impact accuracy? If so, how?
-#   (d) Do any of the trends above change as you change method conditions?
-#   (e) Does the relative accuracy of methods remain the same between the method conditions?
-#   (f) One of the method conditions is "easier" (in that the methods have higher accuracy); which one? 
-#       And what is it about the method condition (i.e., numeric parameters and/or empirical statistics of the sequences that are produced) 
-#       that suggests why that method condition is easier than the other?
-#  3. Describe how the data were generated; what sequence evolution method was used?
-#  4. Which of the methods that you ran is statistically consistent for the method that generated the data? Justify your answers.
-#  5. Did the statistically consistent methods always produce more accurate trees than the methods not guaranteed to be consistent?
-# ==========================================================================================================================================
+# Alignment and Tree Error Dataframe (Wide Data Format)
+# Col:  [0]Iteration   [1]Subset_Size   [2]Initial_Method       [3]GuideTree_Method [4]RFFN     [5]RFFP     [6]SPFN     [7]SPFP      
+# Val:      {1-4}           {10-200}        {clustalo, magus}       {ft, ftnoml}        {0-1}       {0-1}       {0-1}       {0-1}
 # ==========================================================================================================================================
 
 import pandas as pd
 import numpy as np
+import itertools as it
+import os
 
-# ==========================================================================================================================================
-#   1.  (printing out statistics as pandas dataframe)
-# ==========================================================================================================================================
+# iterations  = [1, 2, 3, 4]
+iterations  = [1, 2, 3]
+subsets     = [10, 25, 50, 100, 200]
+initial     = ["magus", "clustalo"]
+guide       = ["ft", "ftnoml"]
 
-replicates = 5
-methods = [
-        "FastTreeGTR",
-        "FastTreeJC",
-        "NeighborJoinGM",
-        "NeighborJoinHam",
-        "NeighborJoinJC",
-]
-models = [
-        "CAT/1000M1",
-        "CAT/1000M4",
-]
+num_replicates = 5
 
-# columns: method, FN/FP R0..., FP/FN average
 data = {}
-data["Method"] = []
-data["Model"] = []
-data[f"FN (avg)"] = []
-data[f"FP (avg)"] = []
-for r in range(replicates):
-    data[f"FN (R{r})"] = []
-    data[f"FP (R{r})"] = []
+data["Iteration"] = []
+data["Subset_Size"] = []
+data["Initial_Method"] = []
+data["GuideTree_Method"] = []
+data["RFFN"] = []
+data["RFFP"] = []
+data["SPFN"] = []
+data["SPFP"] = []
 
-for d in models:
-    for m in methods:
-        data["Method"].append(m)
-        data["Model"].append(d)
-        sum_FN = 0
-        sum_FP = 0
-        for r in range(replicates):
-            filename = f"~/hw6/output/{d}/{m}_R{r}/error.csv"
-            df = pd.read_csv(filename)
-            data[f"FN (R{r})"].append(df["rf_FN"][0])
-            data[f"FP (R{r})"].append(df["rf_FP"][0])
-            sum_FN += df["rf_FN"][0]
-            sum_FP += df["rf_FP"][0]
-        data[f"FN (avg)"].append(sum_FN / replicates)
-        data[f"FP (avg)"].append(sum_FP / replicates)
+for (i, s, t, g) in it.product(iterations, subsets, initial, guide):
+    spfn, spfp, rffn, rffp = 0, 0, 0, 0
+    for r in range(3, 4):
+        sp = f"~/project/output/E1/R{r}/iter{i}/{t}_{s}_{g}_sp.txt"
+        if i == 1:
+            rf = f"~/project/output/E1/R{r}/iter{i}/{t}_rf.csv"
+        else:
+            rf = f"~/project/output/E1/R{r}/iter{i}/{t}_{s}_{g}_rf.csv"
+
+        with open(os.path.expanduser(sp), "r") as f:
+            lines = f.readlines()
+            spfn += float(lines[2].split(" ")[1])
+            spfp += float(lines[3].split(" ")[1])
+
+        with open(os.path.expanduser(rf), "r") as f:
+            lines = f.readlines()
+            rffn += float(lines[1].split(",")[0])
+            rffn += float(lines[1].split(",")[1])
+
+    data["Iteration"].append(i)
+    data["Subset_Size"].append(s)
+    data["Initial_Method"].append(t)
+    data["GuideTree_Method"].append(g)
+    data["RFFN"].append(rffn)
+    data["RFFP"].append(rffp)
+    data["SPFN"].append(spfn)
+    data["SPFP"].append(spfp)
 
 df = pd.DataFrame.from_dict(data)
-df.to_csv("summary_CAT.csv", index=False)
+print(df.head())
+df.to_csv("Results.csv", index=False, float_format='%.4f')
